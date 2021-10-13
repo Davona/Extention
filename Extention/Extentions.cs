6db1b2fbc0a48ea8fb8f88d12e801345c4b95d5f
Davith Shamiryan <dshamiryan@list.ru>
2021-10-13 19:50:11 +0400

    Add project files.

---
+++ Extention/Extentions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extention
+{
+    public static class Extentions
+    {
+        public static string FirstAndLast(this string letter)
+        {
+            string word = "";
+            if (letter.Length == 1)
+            {
+                word = letter;
+            }
+            else
+            {
+                word = word + letter[0] + letter[letter.Length - 1];
+            }
+            return word;
+        }
+        public static string GetLettersOfOddIndex(this string word) 
+        {
+            string letter = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i]%2==0)
+                {
+                    letter = letter + word[i];
+                }
+            }
+            return letter;
+        }
+        public static bool IsNullOrEmpty<T> (this List<T> source ) 
+        {
+            bool result = false;
+            if (source==null||source.Count==0)
+            {
+                result = true;
+            }
+            return result;
+        }
+        public static T GetMiddelValue<T>(this List<T> source) 
+        {
+            T value=default(T);
+            if (source.Count%2!=0)
+            {
+                value = source[source.Count / 2];
+            }
+            else
+            {
+                value = source[source.Count / 2];
+            }
+            return value;
+        }
+        
+
+    }
+}
---
+++ Extention/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Barev";
+            Console.WriteLine(word.FirstAndLast());
+            string newword = word.GetLettersOfOddIndex();
+            Console.WriteLine(newword);
+            List<int> list= new List<int>()
+            { 5, 7,5,8,6,3,9};
+            bool aresult = list.IsNullOrEmpty();
+            int value = list.GetMiddelValue();
+            Console.WriteLine(value);
+        }
+    }
+}
